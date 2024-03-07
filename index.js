030d66a783302a85d120828fa592a1d302b3e9c9
Rylee Jolley <you@example.com>
2024-03-07 15:33:33 -0700

    Created 'shapes.test.js' code and worked out bugs in the 'index.js' to get logo to properly generate. Also downloaded proper versions of jest and inquirer so that terminal testing would function properly.

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-const fs = require(' ')
-const inquirer = require("inquirer");
-const {Circle, Square, Triangle} = require(" ");
+const fs = require('./node_modules/graceful-fs/graceful-fs')
+const inquirer = require('inquirer');
+const {Circle, Square, Triangle} = require("./lib/shapes");
 
-class svg{
+class Svg {
     constructor() {
-        this.textElement = ''
-        this.shapeElement = ''
+        this.textElement = '';
+        this.shapeElement = '';
     }
+
     render() {
-        return `<svg version="1.1" xmlns"http://www.w#.org/2000/svg" width="300" height="200> ${this.shapeElement} ${this.textElement}</svg>`
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`;
     }
-    setTextElement(text,color) {
-        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}"> ${text}</text>`
+
+    setTextElement(text, color) {
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
     }
+
     setShapeElement(shape) {
-        this.shapeElement = shape.render()
+        this.shapeElement = shape.render();
     }
 }
 
@@ -32,7 +35,7 @@ const questions = [
     },
     {
         type: "input",
-        name: "shape",
+        name: "shape-color",
         message: "SHAPE COLOR: Enter a color keyword OR a hexidecimal number:",
     },
     {
@@ -45,66 +48,60 @@ const questions = [
 
 // Function to write data to file
 function writeToFile(fileName, data) {
-    console.log("Writing [" + data + "] to file [" + fileName + "]")
+    console.log('Writing [' + data + '] to file [' + fileName + ']');
     fs.writeFile(fileName, data, function (err) {
         if (err) {
             return console.log(err);
         }
-        console.log("");
+        console.log("You have successfully generated a logo.svg!");
     });
 }
 
 async function init() {
-    console.log("Starting init");
-        var svgString = "";
-        var svg_file = "logo.svg";
-    
+    console.log('Starting init');
     const answers = await inquirer.prompt(questions);
-        var user_text = "";
-        if (answers.text.length > 0 && answers.text.length < 4) {
-            user_text = answers.text;
-        } else {
-            console.log("Invalid user text field detected! Please enter 1-3 characters.");
+    var svgString = '';
+    var svg_file = 'logo.svg';
+    var user_shape_type = answers['pixel-image'];
+    var user_shape_color = answers['shape-color'];
+    var user_font_color = answers['text-color'];
+    var user_text = answers['text'];
+
+    let user_shape;
+    if (user_shape_type === 'Square' || user_shape_type === 'square') {
+        user_shape = new Square();
+        user_shape.setColor(user_shape_color);
+        console.log('Shape color after setting:', user_shape.color);
+        console.log('Selected Square shape');
+    } else if (user_shape_type === 'Circle' || user_shape_type === 'circle') {
+        user_shape = new Circle();
+        user_shape.setColor(user_shape_color);
+        console.log('Shape color after setting:', user_shape.color);
+        console.log('Selected Circle shape');
+    } else if (user_shape_type === 'Triangle' || user_shape_type === 'triangle') {
+        user_shape = new Triangle();
+        user_shape.setColor(user_shape_color);
+        console.log('Shape color after setting:', user_shape.color);
+        console.log('Selected Triangle shape');
+    } else {
+        console.log('Invalid shape!');
         return;
-        }
-            console.log("User text: [" + user_text + "]");
-            user_font_color = answers["text-color"];
+    }
 
-            console.log("User font color: [" + user_font_color + "]");
-            user_shape_color = answers.shape;
+    user_shape.setColor(user_shape_color);
 
-            console.log("User shape color: [" + user_shape_color + "]");
-            user_shape_type = answers["pixel-image"];
-            
-            console.log("User entered shape = [" + user_shape_type + "]");
 
-        let user_shape;
-        if (user_shape_type === "Square" || user_shape_type === "square") {
-            user_shape = new Square();
-            console.log("Selected Square shape");
-        }
-        else if (user_shape_type === "Circle" || user_shape_type === "circle") {
-            user_shape = new Square();
-            console.log("Selected Circle shape");
-        }
-        else if (user_shape_type === "Triangle" || user_shape_type === "triangle") {
-            user_shape = new Square();
-            console.log("Selected Triangle shape");
-        }
-        else {
-            console.log("Invalid shape!");
-        }
-            user_shape.setColor(user_shape_color);
-        
-        var svg = new Svg();
-            svg.setTextElement(user_text, user_font_color);
-            svg.setShapeElement(user_shape);
-            svgString = svg.render();
+    var svg = new Svg();
+    svg.setTextElement(user_text, user_font_color);
+    svg.setShapeElement(user_shape);
+    svgString = svg.render();
 
-        console.log("Displaying shape:\n\n" + svgString);
+    console.log('Displaying shape:\n\n' + svgString);
 
-        console.log("Shape generation complete!");
-        console.log("Writing shape to file...");
-        writeToFile(svg_file, svgString);
-} 
-init()
+    console.log('Shape generation complete!');
+    console.log('Writing shape to file...');
+    writeToFile(svg_file, svgString);
+
+}
+
+init();
